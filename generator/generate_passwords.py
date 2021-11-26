2a10372b17b002cf2d7d33851cb7fc789d67875b
rojo <gayali.rohit@gmail.com>
2021-11-26 12:11:35 -0500

    Add larger files

---
+++ generator/generate_passwords.py
@@ -0,0 +1,21 @@
+import random
+
+charList = input("Enter a list of characters to be used or leave blank to use the default: ")
+
+charList = charList if charList else "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890-_ @*"
+
+pswdList = open("passwords.txt", 'w')
+
+validInput = False;
+while not validInput:
+    try:
+        validInput = True
+        for i in range(int(input("How many passwords should be generated? "))):
+            for j in range(random.randrange(8, 13)):
+                pswdList.write(charList[random.randrange(len(charList))])
+            pswdList.write('\n')
+    except ValueError:
+        validInput = False
+
+pswdList.write(open("common_passwords.min.txt").read())
+pswdList.close()
